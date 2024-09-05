1f6324ffe92413a786bd82c4e0323b30c552a16a
KhadaJhiin <josemiguelortegagaviria@gmail.com>
2024-09-05 16:01:36 -0500

    Se crea proyecto de clientes mercartodo pero con Spring

---
+++ src/main/java/JMDEV/mct_clientes/MctClientesApplication.java
@@ -0,0 +1,13 @@
+package JMDEV.mct_clientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MctClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MctClientesApplication.class, args);
+	}
+
+}
